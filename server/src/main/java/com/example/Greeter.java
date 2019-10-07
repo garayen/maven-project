1cc7d0b298a19dd28a11f9079bd3486d7877c591
Krishna Gupta <krishna.k.gupta@outlook.com>
2019-10-07 23:14:05 +0530

    fixed errors by checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,11 @@ public class Greeter {
 
   }
 
-  public String greet(String someone) {
+ /**
+ * correcting
+ * correction
+ */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
---
+++ server/src/main/java/com/example/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * testing
+ * testign1
+ */
+
